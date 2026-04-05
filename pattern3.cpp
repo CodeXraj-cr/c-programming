7032152f1354c7a7c7278c49f4f0bb42689d0e68
CodeXraj-cr <rajdutts312@gmail.com>
2026-04-05 15:52:53 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int i,j,n,p;
+	
+//	printf("Enter row & col.x..\n");
+//	 scanf("%d %d",&n,&p);
+	
+	for(i=1;i<=5;i++){
+		for(j=1;j<=2*i-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
